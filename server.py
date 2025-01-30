bdb72e4cddd59e7a55b6afcdf50be7c08b50744c
james Shufelt <jshufelt@uark.edu>
2025-01-30 14:00:53 -0600

    started writing componets for server

---
+++ server.py
@@ -0,0 +1,12 @@
+class udp_handler():
+    def __init__():
+        print("lol")
+
+
+class data_base_handler():
+    def __init__():
+        print("lol")
+
+
+def __main__():
+    print("starting server")
