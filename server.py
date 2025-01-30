fb98becd0f3e78791b4435069c413e542e6dda6d
james Shufelt <jshufelt@uark.edu>
2025-01-30 14:47:55 -0600

    wrote function for udp listening

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 
 
-# possibly handle client server com using tcp
+# possibly handle client server com using tcp disscus with team
+
 class tcp_handler:
     def __init__(self):
         print("lol")
@@ -9,7 +10,27 @@ class tcp_handler:
 
 class udp_handler:
     def __init__(self, local_ip: str, local_port: int, buffer_size: int):
-        print("lol")
+        self.local_ip
+        self.local_port
+        self.buffer_size
+        self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.udp_server_socket.bind(local_ip, local_port)
+        print("udp server up and listening")
+
+    # idea make it async
+    def recive_message(self):
+        bytes_address_pair = self.udp_server_socket.recv(self.buffer_size)
+        message = bytes_address_pair[0]
+        address = bytes_address_pair[1]
+        client_msg = "message from client:{}".format(message)
+        client_ip = "client ip:{}".format(address)
+        print(client_msg)
+        print(client_ip)
+
+    def send_message(self, send_message):
+        bytes_to_send = str.encode(send_message)
+
+
 
 
 class data_base_handler():
