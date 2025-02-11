7da2f559fd3ff6184387db3fc86f42a2f8d5b691
RjGutierrezz <rbg002@uark.edu>
2025-02-11 14:57:20 -0600

    fixing

--- server.py
+++ server.py
@@ -1,56 +1,7 @@
 import socket # We use socket inorder to communicate with udp
 
-localIP = "127.0.0.1"
-localPort = 20001
-bufferSize = 1024
-msgFromServer = "Hello UDP Client: "
-bytesToSend   = str.encode(msgFromServer) # encode turns string into bytes
-
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort)) # implementing the socket
-
-print("UDP server up and listening")
-
-# Listen for incoming datagrams
-while(True):
-
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize) # receive both the address and bufferSize
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client: {}".format(message.decode())
-    clientIP  = "Client IP Address: {}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
-
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
-
 # import socket
-
-# local_ip = "127.0.0.1"
-
-
-<<<<<<< HEAD
-# # possibly handle client server com using tcp
-# class tcp_handler:
-#     def __init__(self):
-#         print("lol")
-
-
-# class udp_handler:
-#     def __init__(self, local_ip: str, local_port: int, buffer_size: int):
-#         print("lol")
-=======
 # possibly handle client server com using tcp disscus with team
-
-class tcp_handler:
-    def __init__(self):
-        print("lol")
-
 
 class udp_handler:
     def __init__(self, local_ip: str, local_port: int, buffer_size: int):
@@ -75,18 +26,8 @@ class udp_handler:
     def send_message(self, send_message: str, address: tuple[str, str]):
         bytes_to_send = str.encode(send_message)
         self.udp_server_socket.sendto(bytes_to_send, address)
->>>>>>> f6d43037a174b940cc41093731474be5f61d6d49
 
 
-# class data_base_handler():
-#     def __init__():
-#         print("lol")
-
-
-<<<<<<< HEAD
-# def __main__():
-#     print("starting server")
-=======
 def __main__():
     print("starting server")
     # create udp handler with udp_handler(ip, port, buffer size)
@@ -101,4 +42,3 @@ def __main__():
 
 
 __main__()
->>>>>>> f6d43037a174b940cc41093731474be5f61d6d49
