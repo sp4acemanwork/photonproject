29d53c8f7132b7dcb755eeaa12d8c27a1de943e2
Elmer <elmerpalencia101@gmail.com>
2025-02-13 14:49:42 -0600

    working on displaying random id

--- player_screen.py
+++ player_screen.py
@@ -1,6 +1,8 @@
 from customtkinter import *
 from tkinter import *
 import customtkinter
+import random
+
 
 app = customtkinter.CTk()
 
@@ -10,8 +12,10 @@ app.attributes("-zoomed", True)
 set_appearance_mode("dark")
 
 main_label = customtkinter.CTkLabel(app, width = 10, compound = "center", anchor = "center", text = "PHOTON", font = ('Roboto', 30))
-l1 = Label(app, text = "Player ID")
-l2 = Label(app, text = "Player Name")
+l1_red = Label(app, text = "Player ID")
+l2_red = Label(app, text = "Player Name")
+l1_green = Label(app, text = "Player ID")
+l2_green = Label(app, text = "Player Name")
 
 main_label.grid(row = 0, column = 10)
 
@@ -20,10 +24,38 @@ green_team_label = customtkinter.CTkLabel(app, width = 10, text = "GREEN TEAM", 
 
 
 red_team_label.grid(row = 1, column = 0)
-green_team_label.grid(row = 1, column = 13)
+green_team_label.grid(row = 1, column = 12)
 
-l1.grid(row = 2, column = 1)
-l2.grid(row = 2, column = 2)
+l1_red.grid(row = 2, column = 1)
+l2_red.grid(row = 2, column = 2)
+l1_green.grid(row = 2, column = 13)
+l2_green.grid(row = 2, column = 14) 
+
+
+#CODE FOR GREEN TEAM NAME, ID, # AND TEXT BOXES
+#Code for which player # you are on the green team
+player_num_green = []
+for i in range(15):
+    player_num_green.append(CTkTextbox(app, width = 40, height = 10, border_width = 2, border_color = "green"))
+
+green_num_row = 3
+for item in player_num_green:
+    item.insert(index = "0.0", text = green_num_row - 2)
+    item.configure(state = "disabled")
+    item.grid(row = green_num_row, column = 12, pady = 2)
+    green_num_row += 1
+
+#code for text box that can have an id input for green team
+green_player_id = []
+
+for i in range(15):
+    green_player_id.append(Entry(app))
+
+player_id_row = 3
+
+for item in green_player_id:
+    item.grid(row = player_id_row, column = 13, pady = 2)
+    player_id_row += 1
 
 
 #CODE FOR RED TEAM NAME, ID, #, AND TEXT BOXES 
@@ -40,12 +72,40 @@ for item in player_num_red:
     item.grid(row = num_row, column = 0, pady = 2)
     num_row += 1
 
+
+#code for text box that can have a name input for green team
+green_player_name = []
+
+for i in range(15):
+    green_player_name.append(Entry(app))
+
+player_name_row = 3
+
+for item in green_player_name:
+    item.grid(row = player_name_row, column = 14, padx = 2,pady = 2)
+    player_name_row += 1
+
+
+
+
+
+#red team random generated IDs
+
+transmitting_ids_red = []
+
 #code for text box that can have an id input for red team
 
 player_id = []
 
-for i in range(15):
-    player_id.append(Entry(app))
+for item  in range(15):
+    random_id = random.randint(100000, 999999)
+    print(f"Random ID created: {random_id}")
+    transmitting_ids_red.append(str(random_id))
+
+    player_id.append(CTkTextbox(app, height = 10))
+
+    player_id[item].insert(index = "0.0", text = str(random_id))
+    player_id[item].configure(state = "disabled")
 
 player_id_row = 3
 
@@ -66,11 +126,6 @@ player_name_row = 3
 for item in player_name:
     item.grid(row = player_name_row, column = 2, padx = 2,pady = 2)
     player_name_row += 1
-
-
-
-
-
 
 
 
