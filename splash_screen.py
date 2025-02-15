03e15ee50b43aa2289636e5e74c982ff187c84c2
student <rbg002@uark.edu>
2025-02-15 13:15:03 -0600

    .

--- splash_screen.py
+++ splash_screen.py
@@ -1,4 +1,6 @@
 # import tkinter as tk
+
+import os
 # from tkinter import PhotoImage
 from tkinter import *
 from PIL import ImageTk, Image
@@ -15,21 +17,21 @@ screen_width = splash_root.winfo_screenwidth()
 screen_height = splash_root.winfo_screenheight()
 
 
+# Used os module to get the path of the image dynamically
+image_path = os.path.join(os.path.dirname(__file__), "assets/images/logo.jpg")
+image = Image.open(image_path)
 
-image = Image.open("/Desktop/PhotonProject/photonproject/assets/images/logo.jpg")
 photo = CTkImage(light_image=image, dark_image=image)
 resized_image = CTkImage(light_image=image, size=(screen_width,screen_height))
 
 image_label = CTkLabel(splash_root, image=resized_image, text="" )
 image_label.pack(expand=True)
 
-
 splash_root.bind("<Escape>", lambda e: splash_root.destroy())
-
 
 def next_screen():
     Test()
-    splash_root.destroy
+    splash_root.destroy()
 splash_root.after(3000, next_screen )
 splash_root.mainloop()
 
