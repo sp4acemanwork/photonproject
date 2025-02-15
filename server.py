7d838577a09258c6b5014c0f643a8594162ec864
james Shufelt <jshufelt@uark.edu>
2025-02-15 17:07:45 -0600

    fixed some stuff with naming

--- server.py
+++ server.py
@@ -10,30 +10,31 @@ class tcp_handler:
 
 # you should do all main functions of the back end through this class
 class handler:
-    def __init__(self, local_ip: str, local_port_listen: int, local_port_send: int, buffer_size: int):
-        self.local_ip = local_ip
+    def __init__(self, target_ip: str, local_port_listen: int, local_port_send: int, buffer_size: int):
+        self.target_ip = target_ip
         self.local_port_listen = local_port_listen
         self.local_port_send = local_port_send
         self.buffer_size = buffer_size
-        self.udp_handler = udp_handler(self.local_ip, self.local_port_listen, self.buffer_size)
+        # bind to listen for incoming traffic
+        self.udp_handler = udp_handler("127.0.0.1", self.local_port_listen, self.buffer_size)
 
     def start_game(self):
         print("printing values")
-        self.udp_handler.send_message("202", [self.local_ip, self.local_port_send])
+        self.udp_handler.send_message("202", [self.target_ip, self.local_port_send])
 
     # change ip and port
-    def change_socket(self, local_ip: str, local_port: int):
-        print("changing ip and port from ip:{} port{} -> ip:{} port{}".format(self.local_ip, self.local_port, local_ip, local_port))
+    def change_socket(self, target_ip: str, local_port: int):
+        print("changing ip and port from ip:{} port{} -> ip:{} port{}".format(self.target_ip, self.local_port, target_ip, local_port))
 
     # call thing with address to send?
     # this function will call the add player to database then transit the equipment codes for player?
 
     def add_player(self, player_name: str, player_id: int, equipment_id: int):
         # database_handler.add_player((player_id, player_name))
-        test = (self.local_ip, self.local_port_send)
+        test = (self.target_ip, self.local_port_send)
         self.udp_handler.send_message(str(equipment_id), test)
-        # add check for if user is in the table already
-        # database_handler.print_table()
+        #  add check for if user is in the table already
+        database_handler.print_table()
 
     def recive_message(self) -> tuple[str, str]:
         return self.udp_handler.recive_message()
@@ -41,12 +42,12 @@ class handler:
 
 # this class shouldn't be called directly rather use the functions that do the sending functions automatically
 class udp_handler:
-    def __init__(self, local_ip: str, local_port: int, buffer_size: int):
-        self.local_ip = local_ip
+    def __init__(self, target_ip: str, local_port: int, buffer_size: int):
+        self.target_ip = target_ip
         self.local_port = local_port
         self.buffer_size = buffer_size
         self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.udp_server_socket.bind((self.local_ip, self.local_port))
+        self.udp_server_socket.bind((self.target_ip, self.local_port))
         print("udp server up and listening")
 
     # idea make it async
