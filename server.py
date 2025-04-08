b34686844ca5d2f3d936be51dd345f66f9fd4375
Bwill0104 <bwill0104@gmail.com>
2025-04-08 15:45:17 -0500

    changed it to only use port  7501 and refactoring UI

--- server.py
+++ server.py
@@ -4,17 +4,16 @@ from database import database_handler
 
 # you should do all main functions of the back end through this class
 class handler:
-    def __init__(self, target_ip: str, local_port_listen: int, local_port_send: int, buffer_size: int):
+    def __init__(self, target_ip: str, port: int, buffer_size: int):
         self.target_ip = target_ip
-        self.local_port_listen = local_port_listen
-        self.local_port_send = local_port_send
+        self.port = port
         self.buffer_size = buffer_size
-        self.udp_handler = udp_handler("127.0.0.1", self.local_port_send, self.buffer_size)
+        self.udp_handler = udp_handler("127.0.0.1", self.port, self.buffer_size)
         self.database_handler = database_handler()
 
     def start_game(self):
         print("printing values")
-        self.udp_handler.send_message("202", [self.target_ip, self.local_port_send])
+        self.udp_handler.send_message("202", [self.target_ip, self.port])
 
     # change ip
     def change_socket(self, new_target_ip: str):
@@ -23,7 +22,7 @@ class handler:
         # close original socket and set up new socket
         self.udp_handler.udp_server_socket.close()
         self.udp_handler.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.udp_handler.udp_server_socket.bind((new_target_ip, self.local_port_send))
+        self.udp_handler.udp_server_socket.bind((new_target_ip, self.port))
         self.target_ip = new_target_ip
 
     # call thing with address to send?
@@ -32,7 +31,7 @@ class handler:
     def add_player(self, player_name: str, player_id: int, equipment_id: int):
         player_tuple = (player_id, player_name)
         self.database_handler.add_player(player_tuple)
-        test = (self.target_ip, self.local_port_send)
+        test = (self.target_ip, self.port)
         self.udp_handler.send_message(str(equipment_id), test)
         #  add check for if user is in the table already
         self.udp_handler.recive_message()
